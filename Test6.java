5802294049e1a72766d372959f41eff5ba774b43
Soumyabrata-pc <thesoumyabratac@gmail.com>
2024-04-30 16:44:59 +0530

    1st

---
+++ Test6.java
@@ -0,0 +1,14 @@
+class Test6{
+    int x;
+    int y;
+    int c;
+    public int m1(){
+        c=x+y;
+        return c;
+    }
+    public static void main(String[] args) {
+
+        System.out.println("sum ="+c);// incompleate code....
+        System.out.println("sum ="+c);
+    }
+}
---
+++ test1.java
@@ -0,0 +1,25 @@
+class Test1{
+    
+    /**
+     * @param args
+     */
+
+<<<<<<< HEAD
+     int x=10;
+     int y=20;
+     int c=x+y;
+     
+    public void m5(){
+        
+        System.out.println("chutiya");
+    }
+	
+    public static void main(string[] args)
+    {
+    System.out.println("sum = "+c);
+    System.out.println("SBC");
+    System.out.println("DAS");
+	
+    }
+   
+}
